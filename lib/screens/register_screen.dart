990bcd8478840fc9a9fb54fd8f5b7ed09fb51b5e
anup-paul <anuppaul1845@gmail.com>
2022-01-02 15:15:07 +0600

    registration screen done

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -13,11 +13,93 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Registration"),
-        ),
-        body: const Center(
-          child: Text('This is registration screen'),
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 150,
+                child: Image.asset('assets/images/chat_logo.jpg'),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Enter Your Email",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.lightBlue, width: 3),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.lightBlueAccent, width: 3),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Enter Your Password",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.lightBlue, width: 3),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.lightBlueAccent, width: 3),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Material(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(30),
+                child: MaterialButton(
+                  onPressed: () {
+                    //Navigator.pushNamed(context, '/registrationScreen');
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      'Registration',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
